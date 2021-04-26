515c16d37ff9e1b07938568f48885e91ad4a53c5
biso <bisor0627@gmail.com>
2021-04-26 15:22:38 +0900

    d
    d

--- Project/src/com/javalec/community/Community.java
+++ Project/src/com/javalec/community/Community.java
@@ -1,9 +1,7 @@
 package com.javalec.community;
 
-import java.awt.EventQueue;
 import com.javalec.base.MainMenu;
 
-import javax.swing.JFrame;
 public class Community extends MainMenu {
 
 	public Community() {
